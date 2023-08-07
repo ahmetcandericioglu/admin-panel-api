213c59abad5185e5086c6c58523b41b3a0278990
Ahmet Can DERİCİOĞLU <4hcan2001@gmail.com>
2023-08-07 16:48:39 +0300

    authentication controller oluşturuldu ve login register metodları yazıldı

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,80 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
+{public function __construct()
 {
+    $this->middleware('auth:api', ['except' => ['login', 'register']]);
 }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('username', 'password');
+        $token =  auth()->attempt($credentials);
+
+        if (!$token) {
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'usertitle' => 'required',
+            'username' => 'required|unique:users|alpha:ascii',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'usertitle' => $request->usertitle,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function refresh()
+    {
+        return response()->json([
+            'user' => Auth::user(),
+            'authorisation' => [
+                'token' => Auth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+}
+
